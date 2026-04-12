94abe4ca409b3d7ebd2323c8dfe6dfe42e44e001
Achintya Jain <achintyajain1502@gmail.com>
2026-04-12 09:21:40 +0000

    Updated Repo

--- db.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000
-})
-.then(() => console.log("MongoDB connected ✅"))
-.catch(err => console.error("MongoDB error ❌", err));
